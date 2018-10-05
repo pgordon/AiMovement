a65b77cb2f67304d5cf5b0fd9a60dfaba120ec49
PGordon and/or JCummer <mrs.mattstone@gmail.com>
2018-10-04 23:09:46 -0700

    Arrow keys move coin sprite in two dimensions in simple environment

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float movementAmountPerFrame = 0.03f;
+
+    Rigidbody2D rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPosition = transform.position;
+        if(Input.GetButton("Up"))
+        {
+            newPosition = new Vector2(newPosition.x, newPosition.y + movementAmountPerFrame);
+        }
+        if(Input.GetButton("Down"))
+        {
+            newPosition = new Vector2(newPosition.x, newPosition.y - movementAmountPerFrame);
+        }
+        if (Input.GetButton("Left"))
+        {
+            newPosition = new Vector2(newPosition.x - movementAmountPerFrame, newPosition.y);
+        }
+        if (Input.GetButton("Right"))
+        {
+            newPosition = new Vector2(newPosition.x + movementAmountPerFrame, newPosition.y);
+        }
+        rb.MovePosition(newPosition);
+    }
+}
